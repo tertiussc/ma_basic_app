fd4883b5812741dd8d3fff28aaa34e036e6cccb0
Tertius Scheepers <tertiussc@gmail.com>
2026-05-15 08:45:38 +0200

    only set theme to system theme/default

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.lightTheme, darkTheme: AppTheme.darkTheme, themeMode: ThemeMode.dark, home: AppHome());
+    return MaterialApp(theme: AppTheme.lightTheme, darkTheme: AppTheme.darkTheme, themeMode: ThemeMode.system, home: AppHome());
   }
 }
 
